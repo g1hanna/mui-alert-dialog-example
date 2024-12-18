8ca042e9b765d0b9e1a616e1df9965036fa10026
g1hanna <g1hanna@icloud.com>
2024-12-18 12:20:38 -0500

    Add example code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Stack, Button, Dialog, DialogTitle, Typography, DialogContent, DialogActions, Snackbar, Box, Alert, AlertTitle } from '@mui/material';
 import './App.css';
 
 function App() {
+  const [snackbarOpen, setSnackbarOpen] = useState<boolean>(false);
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Stack direction="row">
+        <Button onClick={() => setModalOpen(true)}>Open Modal</Button>
+      </Stack>
+      <Dialog
+        open={modalOpen}
+        onClose={(_, reason) => {
+          if (reason !== "escapeKeyDown") return;
+          setModalOpen(false);
+        }}
+      >
+        <DialogTitle>
+          <Typography variant="h4" noWrap>
+            Dialog Header
+          </Typography>
+        </DialogTitle>
+        <DialogContent
+          dividers
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            overflowY: "auto",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <Typography>Press &quot;Escape&quot; to close me</Typography>
+        </DialogContent>
+        <DialogActions data-testid="modal-buttons-box" sx={{ p: 2 }}>
+          <Button onClick={() => setSnackbarOpen(true)}>Open Toast</Button>
+        </DialogActions>
+      </Dialog>
+      <Snackbar
+        open={snackbarOpen}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        onClose={(_, reason) => {
+          if (reason !== "escapeKeyDown") return;
+          setSnackbarOpen(false);
+        }}
+      >
+        <Box>
+          <Alert>
+            <AlertTitle sx={{ textAlign: "left" }}>Toast Title</AlertTitle>
+            Press &quot;Escape&quot; to close me
+          </Alert>
+        </Box>
+      </Snackbar>
     </div>
   );
 }
